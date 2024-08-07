1362cd19478128f4a524d0f620b9ec0aeee78571
Sonakshi632 <ssonakshi754@gmail.com>
2024-08-07 23:03:51 +0530

    Add files via upload
    JAVA

---
+++ program2.java
@@ -0,0 +1,14 @@
+//area of circle
+package Demo;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		int r=34;
+		double pi=3.14;
+		double area=pi*r*r;
+		System.out.println("area is"+area);
+
+	}
+
+}
---
+++ program3.java
@@ -0,0 +1,24 @@
+package Demo;
+
+public class program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        int a = 10;
+		        int b = 20;
+		        int c = 30;
+		        
+		        int greatest = a;
+		        
+		        if (b > greatest) {
+		            greatest = b;
+		        }
+		        
+		        if (c > greatest) {
+		            greatest = c;
+		        }
+		        
+		        System.out.println("The greatest number is: " + greatest);
+		    }
+		}
