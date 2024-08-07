2f48a3f94caa729f84078d1022fd58ed8e28b474
Sonakshi632 <ssonakshi754@gmail.com>
2024-08-07 23:12:27 +0530

    Add files via upload

--- program4.java
+++ program4.java
@@ -1,5 +1,26 @@
-package Demo;
+
+
+
+package Demo; 
+import java.util.*;
 
 public class program4 {
-
+    public static void main(String[] args) {
+        int a, b, c;
+        
+      
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.println("Enter first number:");
+        a = sc.nextInt();
+        
+        System.out.println("Enter second number:");
+        b = sc.nextInt();
+        
+        c = a + b;
+        
+       
+        System.out.println("The sum is: " + c);
+    }
 }
